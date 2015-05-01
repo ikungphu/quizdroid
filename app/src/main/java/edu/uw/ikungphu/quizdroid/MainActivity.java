334ca93463c14f9336514957695709eeebe543a7
Preston <ikungphu@uw.edu>
2015-05-01 16:18:06 -0700

    initial commit

---
+++ app/src/main/java/edu/uw/ikungphu/quizdroid/MainActivity.java
@@ -0,0 +1,63 @@
+package edu.uw.ikungphu.quizdroid;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public String[] quizTopics = {"Math", "Physics", "Marvel Super Heroes"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, quizTopics);
+
+        ListView topics = (ListView) findViewById(R.id.topics);
+        topics.setAdapter(itemsAdapter);
+
+        topics.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent info = new Intent(MainActivity.this, quizOverview.class);
+                info.putExtra("topic", quizTopics[position]);
+
+                String topicRes = quizTopics[position].split(" ")[0];
+                info.putExtra("topicRes", topicRes.toLowerCase());
+                startActivity(info);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/edu/uw/ikungphu/quizdroid/quizOverview.java
@@ -0,0 +1,75 @@
+package edu.uw.ikungphu.quizdroid;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class quizOverview extends ActionBarActivity {
+
+    private String topic;
+    private String topicRes;
+    private int numQuestions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_overview);
+
+        Intent launchIntent = getIntent();
+        topic = launchIntent.getStringExtra("topic");
+        topicRes = launchIntent.getStringExtra("topicRes");
+
+        setTitle(topic + "Overview");
+
+        int idTopic = getResources().getIdentifier(topicRes + "_overview", "string", getPackageName());
+        String overview = getResources().getString(idTopic);
+        TextView textOverview = (TextView) findViewById(R.id.textOverview);
+        textOverview.setText(overview);
+
+        final int idNumQuestions = getResources().getIdentifier(topicRes + "_questions", "integer", getPackageName());
+        numQuestions = getResources().getInteger(idNumQuestions);
+        TextView viewNumQuestions = (TextView) findViewById(R.id.numQuestions);
+        viewNumQuestions.setText("Questions: " + numQuestions);
+
+        Button beginButton = (Button) findViewById(R.id.beginBtn);
+        beginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent question = new Intent(quizOverview.this, quizQuestion.class);
+                question.putExtra("topic", topic);
+                question.putExtra("topicRes", topicRes);
+                question.putExtra("numQuestions", numQuestions);
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_quiz_overview, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
