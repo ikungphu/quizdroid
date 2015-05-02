2d375a497a9e843c820784324ae785280dc85cbc
Preston <ikungphu@uw.edu>
2015-05-01 20:58:25 -0700

    second commit

--- app/src/main/java/edu/uw/ikungphu/quizdroid/quizOverview.java
+++ app/src/main/java/edu/uw/ikungphu/quizdroid/quizOverview.java
@@ -45,6 +45,8 @@ public class quizOverview extends ActionBarActivity {
                 question.putExtra("topic", topic);
                 question.putExtra("topicRes", topicRes);
                 question.putExtra("numQuestions", numQuestions);
+                question.putExtra("questionNum", 1);
+                question.putExtra("correct", 0);
 
                 startActivity(question);
             }
--- app/src/main/java/edu/uw/ikungphu/quizdroid/quizQuestion.java
+++ app/src/main/java/edu/uw/ikungphu/quizdroid/quizQuestion.java
@@ -1,18 +1,90 @@
 package edu.uw.ikungphu.quizdroid;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class quizQuestion extends ActionBarActivity {
+
+    private String topic, topicRes;
+    private int numQuestions, questionNum, correct;
+    RadioGroup answers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_question);
+
+        Intent launchIntent = getIntent();
+        topic = launchIntent.getStringExtra("topic");
+        topicRes = launchIntent.getStringExtra("topicRes");
+        numQuestions = launchIntent.getIntExtra("numQuestions", -1);
+        questionNum = launchIntent.getIntExtra("questionNum", -1);
+        correct = launchIntent.getIntExtra("correct", -1);
+
+        answers = (RadioGroup) findViewById(R.id.answers);
+
+        setTitle(topic + "Question " + questionNum);
+
+        TextView correctText = (TextView) findViewById(R.id.correctText);
+        correctText.setText("You have " + correct + " out of " + (questionNum - 1) + " correct.");
+
+        int idQuestion = getResources().getIdentifier(topicRes + "_q" + questionNum, "string", getPackageName());
+        String question = getResources().getString(idQuestion);
+        TextView questionText = (TextView) findViewById(R.id.questionText);
+        questionText.setText(question);
+
+        for (int i = 1; i <= getResources().getInteger(R.integer.questions); i++) {
+            int idAnswer = getResources().getIdentifier(topicRes + "_q" + questionNum + "_a" + i,
+                    "string", getPackageName());
+            String answer = getResources().getString(idAnswer);
+            int idAnswerButton = getResources().getIdentifier("answer" + i, "id", getPackageName());
+            TextView answerButton = (TextView) findViewById(idAnswerButton);
+            answerButton.setText(answer);
+        }
+
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int indexSelected = answers.indexOfChild(findViewById(answers.getCheckedRadioButtonId()));
+
+                if (indexSelected >= 0) {
+                    Intent answer = new Intent(quizQuestion.this, quizAnswer.class);
+
+                    int idCorrectIndex = getResources().getIdentifier(topicRes + "_q" + questionNum + "_a", "integer", getPackageName());
+                    int correctIndex = getResources().getInteger(idCorrectIndex);
+
+                    answer.putExtra("topic", topic);
+                    answer.putExtra("topicRes", topicRes);
+                    answer.putExtra("indexSelected", indexSelected);
+                    answer.putExtra("indexCorrect", correctIndex);
+                    answer.putExtra("numQuestions", numQuestions);
+                    answer.putExtra("questionNum", questionNum);
+
+                    if (1 + indexSelected == correctIndex) {
+                        answer.putExtra("correct", correct + 1);
+                    } else {
+                        answer.putExtra("correct", correct);
+                    }
+
+                    startActivity(answer);
+                    finish();
+                }
+            }
+
+        });
     }
+
 
 
     @Override
